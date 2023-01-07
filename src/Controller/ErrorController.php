d928b5afaa164ca9e969940e9e67be6661bfa5d9
Pascal Keimel <pascal.keimel@apwp.de>
2023-01-07 18:19:43 +0100

    disable custom error controller

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -5,12 +5,10 @@ namespace App\Controller;
 use Exception;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Route;
-use Throwable;
 
 class ErrorController extends AbstractController
 {
-    #[Route('/error', name: 'error', methods: ['GET'])]
-    public function show(\Exception $exception): Response
+    public function show(Exception $exception): Response
     {
         return new Response($exception->getMessage() . "<br />" . $exception->getTraceAsString());
     }
