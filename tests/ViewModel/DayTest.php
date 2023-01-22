d89cf334748d8075821eedf7159d9d4eebd97430
Pascal Keimel <pascal.keimel@apwp.de>
2023-01-22 17:15:40 +0100

    show holydays for saxony  in schedule

--- tests/ViewModel/DayTest.php
+++ tests/ViewModel/DayTest.php
@@ -21,7 +21,7 @@ final class DayTest extends TestCase
      */
     public function testgetDayShortName(Day $day): void
     {
-        $this->assertEquals('Mon', $day->getDayShortName());
+        $this->assertEquals('Mo.', $day->getDayShortName());
     }
 
     /**
@@ -51,6 +51,36 @@ final class DayTest extends TestCase
             [new Day(new \DateTimeImmutable('2022-08-06')), true], // Saturday
             [new Day(new \DateTimeImmutable('2022-08-07')), true], // Sunday
             [new Day(new \DateTimeImmutable('2022-08-08')), false], // Monday
+        ];
+    }
+
+    /**
+     * @dataProvider isHolidayProvider
+     */
+    public function testisHolyday(Day $day, bool $expectedResult): void
+    {
+        $this->assertEquals($expectedResult, $day->isHolyday());
+    }
+
+    public function isHolidayProvider(): array
+    {
+        return [
+
+            [new Day(new \DateTimeImmutable('2022-01-01')), true], // new year
+            [new Day(new \DateTimeImmutable('2022-01-02')), false], // no holiday
+            [new Day(new \DateTimeImmutable('1974-04-12')), true], // Easter Friday
+            [new Day(new \DateTimeImmutable('1974-04-13')), false], // no holiday - Easter Saturday
+            [new Day(new \DateTimeImmutable('1974-04-14')), true], // Easter Sunday
+            [new Day(new \DateTimeImmutable('1974-04-15')), true], // Easter Monday
+            [new Day(new \DateTimeImmutable('2023-05-18')), true], // trip to heaven
+            [new Day(new \DateTimeImmutable('2023-05-29')), true], // Pentecost
+            [new Day(new \DateTimeImmutable('2019-05-01')), true], // day of work!
+            [new Day(new \DateTimeImmutable('2020-10-03')), true], // reunion day
+            [new Day(new \DateTimeImmutable('2021-10-31')), true], // reformation day
+            [new Day(new \DateTimeImmutable('2022-11-16')), true], // bed and bus day
+            [new Day(new \DateTimeImmutable('2023-12-25')), true], // chrismas 1
+            [new Day(new \DateTimeImmutable('2024-12-26')), true], // chrismas 2
+            [new Day(new \DateTimeImmutable('2024-12-31')), false], // no holiday
         ];
     }
 }
