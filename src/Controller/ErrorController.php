73d07f206bf71d6200d319908bb115cae2b324d8
Pascal Keimel <pascal.keimel@apwp.de>
2023-02-04 17:40:51 +0100

    introduce an .env.example file

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use Exception;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Route;
+use Throwable;
 
 class ErrorController extends AbstractController
 {
-    public function show(Exception $exception): Response
+    public function show(Throwable $exception): Response
     {
         return new Response($exception->getMessage() . "<br />" . $exception->getTraceAsString());
     }
