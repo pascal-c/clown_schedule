bf9edd0a330bc0d8ad6dbc9c1d3204110aa3d047
Pascal Keimel <pascal.keimel@apwp.de>
2024-01-23 17:22:37 +0100

    added github action test

--- src/Gateway/VacationGateway.php
+++ src/Gateway/VacationGateway.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Gateway;
 
+use Symfony\Component\HttpClient\Exception\ClientException;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
@@ -19,24 +20,27 @@ class VacationGateway
 
     public function findByYear(string $year): array
     {
-        try {
-            $content = $this->vacationCache->get(
-                'vacations '.$year,
-                function (ItemInterface $item) use ($year) {
-                    $item->expiresAfter(3600 * 24 * 30);
-
+        return $this->vacationCache->get(
+            'vacations '.$year,
+            function (ItemInterface $item) use ($year): array {
+                try {
                     $response = $this->httpClient->request(
                         'GET',
-                        'https://ferien-api.de/api/v1/holidays/SN/'.$year
+                        'https://ferien-api.de/api/v1/holidays/SN/'.$year,
+                        [
+                            'timeout' => 2,
+                            'max_duration' => 2,
+                        ]
                     );
+                    $item->expiresAfter(3600 * 24 * 30); // keep for 30 days
 
                     return $response->toArray();
-                }
-            );
-        } catch (TransportExceptionInterface $exception) {
-            $content = [];
-        }
+                } catch (TransportExceptionInterface|ClientException) {
+                    $item->expiresAfter(10); // something went wrong - retry after 10s
 
-        return $content;
+                    return [];
+                }
+            }
+        );
     }
 }
