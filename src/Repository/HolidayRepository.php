10175249078fc747b74f8b5ccf734f0ba8f63c9a
Pascal Keimel <pascal.keimel@apwp.de>
2025-02-14 17:22:43 +0100

    use configured state for showing holidays in calendar

--- src/Repository/HolidayRepository.php
+++ src/Repository/HolidayRepository.php
@@ -9,7 +9,7 @@ use DateTimeZone;
 
 class HolidayRepository
 {
-    private array $cache;
+    private array $cache = [];
 
     public function __construct(private ConfigRepository $configRepository)
     {
@@ -32,18 +32,23 @@ class HolidayRepository
         $busAndBedDate = (new DateTimeImmutable($year.'-11-23'))->modify('last Wednesday');
 
         return [
-            $year.'-01-01' => 'Neujahr', // new year
-            $year.'-05-01' => 'Tag der Arbeit', // day of work!
-            $year.'-10-03' => 'Tag der deutschen Einheit', // reunion day
-            $year.'-10-31' => 'Reformationstag', // reformation day
-            $year.'-12-25' => '1. Weihnachtsfeiertag', // chrismas 1
-            $year.'-12-26' => '2. Weihnachtsfeiertag', // chrismas 2
-            $easterDate->format('Y-m-d') => 'Ostersonntag', // easter
-            $easterDate->modify('-2 days')->format('Y-m-d') => 'Karfreitag', // easter friday
-            $easterDate->modify('+1 day')->format('Y-m-d') => 'Ostermontag', // easter monday
-            $easterDate->modify('+39 day')->format('Y-m-d') => 'Himmelfahrt', // trip to heaven
-            $easterDate->modify('+50 day')->format('Y-m-d') => 'Pfingsten', // pentercote
-            $busAndBedDate->format('Y-m-d') => 'Buß- und Bettag', // bus and bed day
+            $year.'-01-01' => 'Neujahr',                                                            // new year
+            $year.'-01-06' => in_array($federalState, ['BW', 'BY', 'ST']) ? 'Drei Könige' : null,   // holy three kings
+            $year.'-03-08' => 'BE' === $federalState ? 'Frauentag' : null,                          // womens day
+            $year.'-05-01' => 'Tag der Arbeit',                                                     // day of work!
+            $year.'-08-15' => in_array($federalState, ['BY', 'SL']) ? 'Mariä Himmelfahrt' : null,   // Maria's trip to heaven
+            $year.'-10-03' => 'Tag der deutschen Einheit',                                          // reunion day
+            $year.'-10-31' => in_array($federalState, ['BB', 'HB', 'HH', 'MV', 'NI', 'SN', 'ST', 'SH', 'TH']) ? 'Reformationstag' : null, // reformation day
+            $year.'-11-01' => in_array($federalState, ['BW', 'BY', 'NW', 'RP', 'SL']) ? 'Allerheiligen' : null, // all saints
+            $year.'-12-25' => '1. Weihnachtsfeiertag',                                              // chrismas 1
+            $year.'-12-26' => '2. Weihnachtsfeiertag',                                              // chrismas 2
+            $easterDate->format('Y-m-d') => 'Ostersonntag',                                         // easter
+            $easterDate->modify('-2 days')->format('Y-m-d') => 'Karfreitag',                        // easter friday
+            $easterDate->modify('+1 day')->format('Y-m-d') => 'Ostermontag',                        // easter monday
+            $easterDate->modify('+39 day')->format('Y-m-d') => 'Himmelfahrt',                       // trip to heaven
+            $easterDate->modify('+50 day')->format('Y-m-d') => 'Pfingsten',                         // pentercote
+            $easterDate->modify('+60 day')->format('Y-m-d') => in_array($federalState, ['BW', 'BY', 'HE', 'NW', 'RP', 'SL']) ? 'Fronleichnam' : null, // Corpus Christi
+            $busAndBedDate->format('Y-m-d') => 'SN' === $federalState ? 'Buß- und Bettag' : null,    // bus and bed day
         ];
     }
 }
--- tests/phpunit/Repository/HolidayRepositoryTest.php
+++ tests/phpunit/Repository/HolidayRepositoryTest.php
@@ -22,13 +22,13 @@ final class HolidayRepositoryTest extends TestCase
         $this->repository = new HolidayRepository($this->configRepository);
     }
 
-    #[DataProvider('isHolidayProvider')]
-    public function testOneByDate(DateTimeImmutable $date, bool $isHoliday, ?string $holidayName): void
+    #[DataProvider('HolidayProviderSN')]
+    public function testOneByDateForSaxony(DateTimeImmutable $date, bool $isHoliday, ?string $holidayName): void
     {
         $this->configRepository
             ->expects($this->once())
             ->method('getFederalState')
-            ->willReturn('NW');
+            ->willReturn('SN');
 
         $holidayName = $this->repository->oneByDate($date);
         $this->assertSame($isHoliday, null !== $holidayName);
@@ -38,24 +38,63 @@ final class HolidayRepositoryTest extends TestCase
         $this->assertSame($holidayName, $holidayName);
     }
 
-    public static function isHolidayProvider(): array
+    public static function holidayProviderSN(): array
     {
         return [
             [new DateTimeImmutable('2022-01-01'), true, 'Neujahr'], // new year
-            [new DateTimeImmutable('2022-01-02'), false, null], // no holiday
+            [new DateTimeImmutable('2022-01-06'), false, null], // three kings - no holiday in SN
             [new DateTimeImmutable('1974-04-12'), true, 'Karfreitag'], // Easter Friday
             [new DateTimeImmutable('1974-04-13'), false, null], // no holiday - Easter Saturday
             [new DateTimeImmutable('1974-04-14'), true, 'Ostersonntag'], // Easter Sunday
             [new DateTimeImmutable('1974-04-15'), true, 'Ostermontag'], // Easter Monday
             [new DateTimeImmutable('2023-05-18'), true, 'Himmelfahrt'], // trip to heaven
             [new DateTimeImmutable('2023-05-29'), true, 'Pfingsten'], // Pentecost
+            [new DateTimeImmutable('2023-06-08'), false, null], // Corpus Christi - no holiday in SN
             [new DateTimeImmutable('2019-05-01'), true, 'Tag der Arbeit'], // day of work!
             [new DateTimeImmutable('2020-10-03'), true, 'Tag der deutschen Einheit'], // reunion day
             [new DateTimeImmutable('2021-10-31'), true, 'Reformationstag'], // reformation day
             [new DateTimeImmutable('2022-11-16'), true, 'Buß- und Bettag'], // bed and bus day
             [new DateTimeImmutable('2023-12-25'), true, '1. Weihnachtsfeiertag'], // chrismas 1
             [new DateTimeImmutable('2024-12-26'), true, '2. Weihnachtsfeiertag'], // chrismas 2
-            [new DateTimeImmutable('2024-12-31'), false, null], // no holiday
+            [new DateTimeImmutable('2024-12-31'), false, null], // sylvester - no holiday
+        ];
+    }
+
+    #[DataProvider('HolidayProviderHE')]
+    public function testOneByDateForHessen(DateTimeImmutable $date, bool $isHoliday, ?string $holidayName): void
+    {
+        $this->configRepository
+            ->expects($this->once())
+            ->method('getFederalState')
+            ->willReturn('HE');
+
+        $holidayName = $this->repository->oneByDate($date);
+        $this->assertSame($isHoliday, null !== $holidayName);
+
+        // call againg to test that the cache works
+        $holidayName = $this->repository->oneByDate($date);
+        $this->assertSame($holidayName, $holidayName);
+    }
+
+    public static function holidayProviderHE(): array
+    {
+        return [
+            [new DateTimeImmutable('2022-01-01'), true, 'Neujahr'], // new year
+            [new DateTimeImmutable('2022-01-06'), false, null], // three kings - no holiday in HE
+            [new DateTimeImmutable('1974-04-12'), true, 'Karfreitag'], // Easter Friday
+            [new DateTimeImmutable('1974-04-13'), false, null], // no holiday - Easter Saturday
+            [new DateTimeImmutable('1974-04-14'), true, 'Ostersonntag'], // Easter Sunday
+            [new DateTimeImmutable('1974-04-15'), true, 'Ostermontag'], // Easter Monday
+            [new DateTimeImmutable('2023-05-18'), true, 'Himmelfahrt'], // trip to heaven
+            [new DateTimeImmutable('2023-05-29'), true, 'Pfingsten'], // Pentecost
+            [new DateTimeImmutable('2023-06-08'), true, 'Fronleichnam'], // Corpus Christi
+            [new DateTimeImmutable('2019-05-01'), true, 'Tag der Arbeit'], // day of work!
+            [new DateTimeImmutable('2020-10-03'), true, 'Tag der deutschen Einheit'], // reunion day
+            [new DateTimeImmutable('2021-10-31'), false, null], // reformation day - no holiday in HE
+            [new DateTimeImmutable('2022-11-16'), false, null], // bed and bus day - no holiday in HE
+            [new DateTimeImmutable('2023-12-25'), true, '1. Weihnachtsfeiertag'], // chrismas 1
+            [new DateTimeImmutable('2024-12-26'), true, '2. Weihnachtsfeiertag'], // chrismas 2
+            [new DateTimeImmutable('2024-12-31'), false, null], // sylvester - no holiday
         ];
     }
 }
